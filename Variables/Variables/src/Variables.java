f304955e5e992304856a796af4eb30ace64bf42c
gustavoalves23 <gustavo.alves388@gmail.com>
2022-05-17 09:35:35 -0300

    feat: Basic variables

---
+++ Variables/Variables/src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myInteger = 10;
+        System.out.println("Integer: " + myInteger);
+
+        float myFloat = 1.333f;
+        System.out.println("Float: " + myFloat);
+
+        float mySum = myInteger + myFloat;
+        System.out.println("Sum: " + mySum);
+
+        String myString = "Hello!";
+        System.out.println("String: " + myString);
+    }
+}
